9100616c9653a9d77d256a78c328eef19cfb4997
Ričards Cacurs <rcacurs@gmail.com>
2015-12-01 12:16:56 +0200

    creating method for ellipsoid fittin

---
+++ calibration-java-app/src/main/java/lv/edi/SmartWearCalibJavaApp/Main.java
@@ -0,0 +1,4 @@
+package lv.edi.SmartWearCalibJavaApp;
+
+public class Main {
+}
--- smartwear-processing-lib/src/main/java/lv/edi/SmartWearProcessing/Calibration.java
+++ smartwear-processing-lib/src/main/java/lv/edi/SmartWearProcessing/Calibration.java
@@ -1,7 +1,18 @@
 package lv.edi.SmartWearProcessing;
+import org.ejml.*;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
 
 /**
  * Created by Richards on 01.12.2015..
  */
 public class Calibration {
+    /**
+     * Function for fitting data to elipsoid for sensor data calibration.
+     * @param inputData DenseMatrix64F n x 3 matrix, (n rows). each n is one measurement
+     */
+    public static void ellipsoidFitCalibration(DenseMatrix64F inputData){
+        CommonOps.extract(inputData, 0, inputData.numRows, 0, 1);
+
+    }
 }
